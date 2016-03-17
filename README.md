0b7cb78203e33ae63137f0b68e31cd5fd29988ba
Matúš Petrofčík <petrofcikmatus@gmail.com>
2016-03-17 06:45:34 +0100

    app

---
+++ README.md
@@ -0,0 +1,7 @@
+# README #
+
+This is my homepage at [www.petrofcik.eu](http://www.petrofcik.eu) :)
+
+Hosted by [GitHub Pages](https://pages.github.com) directly from [petrofcikmatus.github.io repository](https://github.com/petrofcikmatus/petrofcikmatus.github.io).
+
+Thanks to [Mr. Igor Kulman](https://www.kulman.sk) for this idea at [his blog](https://www.kulman.sk/sk/content/navrat-statickeho-webu).
---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+(function (window, document, $, undefined) {
+    $(document).ready(function () {
+        // hide charmander loading
+        $('.loading-container').fadeOut();
+
+        // set GA triggers
+        $('.ga-mailto').on('click', function(){
+            ga('send', 'event', 'link', 'mailto');
+        });
+
+        $('.ga-tel').on('click', function(){
+            ga('send', 'event', 'link', 'tel');
+        });
+
+        $('.ga-itnetwork.cz').on('click', function(){
+            ga('send', 'event', 'link', 'itnetwork.cz');
+        });
+
+        $('.ga-webissimo.sk').on('click', function(){
+            ga('send', 'event', 'link', 'webissimo.sk');
+        });
+
+        $('.ga-fvojtas.com').on('click', function(){
+            ga('send', 'event', 'link', 'fvojtas.com');
+        });
+
+        $('.ga-other').on('click', function(){
+            ga('send', 'event', 'link', 'other');
+        });
+    });
+})(window, document, jQuery);
