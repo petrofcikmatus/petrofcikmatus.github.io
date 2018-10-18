d527f238749ed16864a350abfe0c4c6d276d6171
Matus Petrofcik <petrofcikmatus@gmail.com>
2018-10-18 22:10:41 +0200

    Reformat javascript code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
 
-        var target = this.hash;
-        var $target = $(target);
-        var $nav = $('.nav');
+    var target = this.hash;
+    var $target = $(target);
+    var $nav = $('.nav');
 
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - $nav.height()
-        }, 900, 'swing');
-    });
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - $nav.height()
+    }, 900, 'swing');
+  });
 
-    $('.section__paragraph').each(function () {
-        var str = $(this).html();
-        str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi, '$1$2&nbsp;$4');
-        $(this).html(str);
-    });
+  $('.section__paragraph').each(function () {
+    var str = $(this).html();
+    str = str.replace(/(\s|^)(a|i|k|o|s|u|v|z)(\s+)([^\p{Cc}\p{Cf}\p{zL}\p{Zp}]+)/gmi, '$1$2&nbsp;$4');
+    $(this).html(str);
+  });
 });
